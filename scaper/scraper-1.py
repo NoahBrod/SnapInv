df61a93b898bf730eff10406ca38e8439b89ce4c
Noah Brodke <noahbrodke@gmail.com>
2024-06-04 04:00:23 -0400

    Search Working
    For the 2 books I'm testing this works perfect. Will probably need changes for other stuff.

--- scaper/scraper-1.py
+++ scaper/scraper-1.py
@@ -30,44 +30,113 @@ from flask import Flask, request, jsonify
 # print(urls)
 app = Flask(__name__)
 
+markers = "/-_"
+
+def findKeyWord(i):
+    print()
+    highIndex = 0
+        
+    for j in markers:
+        curIndex = i.find(j)
+        if curIndex > 0:
+            if curIndex < highIndex and highIndex != 0:
+                highIndex = curIndex
+                print("Marker index at: " + str(highIndex))
+                break
+            else:
+                highIndex = curIndex
+    
+    if highIndex == 0:
+        return "NONE"
+    
+    return i[:highIndex]
+
+def searchKeyWord(i, urls, keyWord):
+    matches = 1
+    for j in urls:
+            if i != j:
+                if keyWord.lower() in j.lower():
+                    print("Found keyword: " + keyWord)
+                    matches += 1
+                
+                # if matches > 2:
+                #     keyWords.append(keyWord)
+                #     break
+    # print()
+    return matches
+
 @app.route('/receive_code', methods=['POST'])
 def receive_code():
     data = request.json
     print("Received data:", data)
     
     urls = []
-
-    query = "811628030030"
-    print()
-    i = 0
-    for j in search(query, num=3, stop=3, pause=2):
-        print(j)
-        urls.append(j)
-        i += 1
-
-    # put item name finding here
-    if 's' in urls[0]:
-        print('s was found')
-    for j in urls:
-        builtString = ""
-        j = j.substring()
-        print("j:"+j)
-        for k in j:
-            # print(k)
-            for l in urls:
-                print("l:"+l)
-                if l != j:
-                    print("l is not j")
-                    if k in l:
-                        builtString += k
-    print(builtString)
-    # scraping found pages
-
-
-    # for j in urls:
-    #     print()
+    fixedUrls = []
+    keyWords = []
     
-    return urls
+    finalString = ""
+
+    query = "9780670922857"
+    print("Searching: " + query)
+    for i in search(query, num=7, stop=7, pause=2):
+        urls.append(i)
+    
+    print()
+    print("Found:")
+    for i in urls:
+        print("    " + i)
+    
+    print()
+    print("Changing urls:")
+    for i in range(len(urls)):
+        urlFix = urls[i][urls[i].index("//")+2:]
+        urlFix = urlFix[urlFix.index("/")+1:]
+        fixedUrls.append(urlFix)
+        print("    " + urlFix)
+    
+    print()
+    print("Getting key words:")
+    
+    i = 0
+    count = 0
+    while True:
+        print()
+        keyWord = findKeyWord(fixedUrls[i])
+        if keyWord != query:
+            matches = searchKeyWord(fixedUrls[i], fixedUrls, keyWord)
+            if matches > 2 and not keyWord in keyWords:
+                keyWords.append(keyWord)
+            
+        if findKeyWord(fixedUrls[i]):
+            if keyWord != "NONE":
+                fixedUrls[i] = fixedUrls[i][len(keyWord)+1:]
+                print("Key word " + keyWord + " removed: " + fixedUrls[i])
+            else:
+                i += 1
+            
+            count += 1
+            
+        if i == len(fixedUrls):
+            break
+    
+    print(keyWords)
+    lowerKeyWords = map(lambda x:x.lower(), keyWords)
+    presentKeyWords = []
+    for i in keyWords:
+        check = False
+        if i.lower() in lowerKeyWords and i.lower() not in presentKeyWords:
+            if len(i) == 1:
+                for j in keyWords:
+                    if len(j) > 1 and i in j:
+                        presentKeyWords.append(i.lower())
+                        check = True
+                        break
+            if not check:
+                presentKeyWords.append(i.lower())
+                finalString += i + " "
+    finalString = finalString[:len(finalString)-1]
+    
+    return finalString
 
 if __name__ == '__main__':
     app.run(debug=True)
