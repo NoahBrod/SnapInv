2b58e2f9ee7e8a095496c4273e9f16b89dd75106
Noah Brodke <noahbrodke@gmail.com>
2024-08-08 01:59:10 -0400

    Update

--- snapinv/src/main/java/com/dmn/snapinv/web/WebController.java
+++ snapinv/src/main/java/com/dmn/snapinv/web/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
-    @GetMapping("/")
+    @GetMapping({"/", "/home"})
     public String home() {
         return "index";
     }
