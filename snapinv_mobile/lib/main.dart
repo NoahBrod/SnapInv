4f5cca908439031b8abfccefb89dd7b772938ce2
Noah Brodke <73453253+NoahBrod@users.noreply.github.com>
2024-09-18 11:13:32 -0400

    Started App

--- snapinv_mobile/lib/main.dart
+++ snapinv_mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,15 +10,90 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: [
-            Center(),
-            Text('Hello World!'),
-            Text('Hello World!'),
-          ],
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.dmSansTextTheme(),
+        // scaffoldBackgroundColor: const Color.fromRGBO(35, 214, 128, 1),
+      ),
+      home: BottomNav(),
+    );
+  }
+}
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static const List<Widget> _widgetOptions = <Widget>[
+    Scaffold(
+          body: Center(
+            child: Column(
+              // mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(50.0),
+                  child: Text(
+                    style: TextStyle(fontSize: 30),
+                    textAlign: TextAlign.center,
+                    'Welcome to SnapInv',
+                  ),
+                ),
+                Text('s'),
+              ],
+            ),
+          ),
         ),
+        Scaffold(
+
+        ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SnapInv Demo'),
+        backgroundColor: Color.fromRGBO(35, 214, 128, 1),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectedIndex,
+        selectedFontSize: 15,
+        onTap: _onTap,
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard_outlined),
+            activeIcon: Icon(Icons.dashboard),
+            label: 'Dash',
+            backgroundColor: Color.fromRGBO(35, 214, 128, 1),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_outlined),
+            activeIcon: Icon(Icons.camera),
+            label: 'Camera',
+            backgroundColor: Color.fromRGBO(35, 214, 128, 1),
+          ),
+        ],
       ),
     );
   }
