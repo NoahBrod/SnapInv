b53be6ccd65a4b3a3a9e2eaec298c91e8d1fbf37
Noah Brodke <73453253+NoahBrod@users.noreply.github.com>
2024-09-30 16:51:00 -0400

    Adding pages and controllers

--- snapinv_mobile/lib/pages/dashboard.dart
+++ snapinv_mobile/lib/pages/dashboard.dart
@@ -1,39 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import 'pages/dashboard.dart';
+class _DashBoard extends StatefulWidget {
+  const _DashBoard({super.key});
 
-void main() {
-  runApp(const MainApp());
+  @override
+  State<_DashBoard> createState() => _DashBoardState();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class _DashBoardState extends State<_DashBoard>
+    with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        textTheme: GoogleFonts.dmSansTextTheme(),
-        // scaffoldBackgroundColor: const Color.fromRGBO(35, 214, 128, 1),
-      ),
-      home: BottomNav(),
-    );
-  }
-}
-
-class BottomNav extends StatefulWidget {
-  const BottomNav({super.key});
-
-  @override
-  State<BottomNav> createState() => _BottomNavState();
-}
-
-class _BottomNavState extends State<BottomNav> {
-  int _selectedIndex = 2;
-
-  static List<Widget> _widgetOptions = <Widget>[
-    Scaffold(
+    super.build(context);
+    return Scaffold(
       // Dashboard
       body: Center(
         child: Column(
@@ -111,75 +90,9 @@ class _BottomNavState extends State<BottomNav> {
           ],
         ),
       ),
-    ),
-    Scaffold(// Camera
-        ),
-    Scaffold(
-      // Inventory Page
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Navigator.push(context, )
-        },
-        backgroundColor: Color.fromRGBO(35, 214, 128, 1),
-        elevation: 5,
-        child: Icon(Icons.add, color: Colors.white,),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('Item 1'),
-          ),
-        ],
-      ),
-    ),
-  ];
-
-  void _onTap(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    );
   }
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'SnapInv Demo',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Color.fromRGBO(35, 214, 128, 1),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        currentIndex: _selectedIndex,
-        selectedFontSize: 15,
-        onTap: _onTap,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard_outlined),
-            activeIcon: Icon(Icons.dashboard),
-            label: 'Dash',
-            backgroundColor: Color.fromRGBO(35, 214, 128, 1),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera_outlined),
-            activeIcon: Icon(Icons.camera),
-            label: 'Camera',
-            backgroundColor: Color.fromRGBO(35, 214, 128, 1),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shelves),
-            // activeIcon: Icon(Icons.),
-            label: 'Inventory',
-            backgroundColor: Color.fromRGBO(35, 214, 128, 1),
-          ),
-        ],
-      ),
-    );
-  }
+  bool get wantKeepAlive => true;
 }
